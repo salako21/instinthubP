b13a2aa60e0665ffc087aae5ed17c77512241252
salako21 <49339343+salako21@users.noreply.github.com>
2023-03-20 20:56:35 +0100

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,32 @@
+const num1 = 10; 
+const num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+console.log(sum);
+console.log(subt);
+console.log(mult);
+console.log(divd);
+console.log(rem);
+
+num1 = ++num1;
+num2 = --num2;
+num1 = --num1;
+num2 = ++num2;
+
+// to get input from user
+
+var numb1;
+var numb2;
+
+numb1 = prompt("Enter the first Number:");
+numb2 = prompt("Enter the second number:");
+
+var variables = Number(numb1) + Number(numb2);
+alert("The Score is " + variables);
+
+
